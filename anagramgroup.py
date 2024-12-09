d7f8adab1f63a87a1be271d8d70b42cec0bf4d07
sandeep <sandeepmorri569@gmail.com>
2024-12-09 16:15:51 +0530

    webpage

---
+++ anagramgroup.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+def group_anagrams(strs):
+    anagram_map = defaultdict(list)
+
+    for word in strs:
+        # Sort each word and use the sorted word as a key
+        sorted_word = ''.join(sorted(word))
+        anagram_map[sorted_word].append(word)
+    
+    # Return the grouped anagrams as a list of lists
+    return list(anagram_map.values())
+
+# Example usage
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+grouped_anagrams = group_anagrams(strs)
+print(grouped_anagrams)
